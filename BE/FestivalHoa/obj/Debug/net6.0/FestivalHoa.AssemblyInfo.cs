0e4afe6c6d5d59b5e827672c229cf0ea02d4bbbf
phatdatdang <phatdat684@gmail.com>
2025-02-19 08:35:41 +0700

    update2

--- BE/FestivalHoa/obj/Debug/net6.0/FestivalHoa.AssemblyInfo.cs
+++ BE/FestivalHoa/obj/Debug/net6.0/FestivalHoa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FestivalHoa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f22bee21e5eb54fb4afc41e86b2dfab00c7a2fb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FestivalHoa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FestivalHoa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
